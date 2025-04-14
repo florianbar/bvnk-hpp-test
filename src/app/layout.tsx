9b286fbd63bd9cd75036c6017eefb9783f6f2417
Florian Bär <florianbar01@gmail.com>
2025-04-14 10:40:54 +0200

    move components out of app directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
-import QueryProvider from "./components/query-provider";
+import QueryProvider from "@/components/query-provider";
 import "./globals.css";
 
 const geistSans = Geist({
