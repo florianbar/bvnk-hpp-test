14156d28b6ade6be1328986815c6e5569cda979f
Florian Bär <florianbar01@gmail.com>
2025-04-15 15:30:04 +0200

    create quote expired component and styling

--- src/app/payin/[uuid]/expired/page.tsx
+++ src/app/payin/[uuid]/expired/page.tsx
@@ -3,13 +3,18 @@
 import { PayinPageProps } from "@/types/payin";
 import QuotePageHandler from "@/components/quote-page-handler";
 import Container from "@/components/ui/container";
+import QuoteExpired from "@/components/quote-expired";
 
 export default async function ExpiredQuotePage({ params }: PayinPageProps) {
   const { uuid } = await params;
 
   return (
     <QuotePageHandler currentUrl={`/payin/${uuid}/expired`} uuid={uuid}>
-      {() => <Container>Expired</Container>}
+      {() => (
+        <Container>
+          <QuoteExpired />
+        </Container>
+      )}
     </QuotePageHandler>
   );
 }
