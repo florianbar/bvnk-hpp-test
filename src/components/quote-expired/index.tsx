d4e589e865deb4e0fe3988fd8c2a0778322cc1c4
Florian Bär <florianbar01@gmail.com>
2025-04-15 15:56:45 +0200

    update font styling on expiry page

--- src/components/quote-expired/index.tsx
+++ src/components/quote-expired/index.tsx
@@ -2,9 +2,9 @@ import { ExclamationCircleIcon } from "@heroicons/react/24/solid";
 
 export default function QuoteExpired() {
   return (
-    <div className="text-center px-[6rem] py-[3rem]">
+    <div className="text-center px-[5rem] py-[3rem]">
       <ExclamationCircleIcon className="size-[4rem] mx-auto text-bvnk-danger mb-4" />
-      <h2 className="text-2xl font-bold mb-5">Payment details expired</h2>
+      <h2 className="text-2xl font-semibold mb-6">Payment details expired</h2>
       <p className="text-[15px]">
         The payment details for your transaction have expired.
       </p>
